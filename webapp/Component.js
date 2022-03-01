2ad0525805a3c7e3da4bbd2e3265415c771d8a79
kielbo <pytkens@gmail.com>
2022-03-01 12:09:35 +0100

    panel added

--- webapp/Component.js
+++ webapp/Component.js
@@ -6,13 +6,10 @@ sap.ui.define([
     "use strict";
     return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
         metadata: {
-            "interfaces": ["sap.ui.core.IAsyncContentCreation"],
-            "rootView": {
-                "viewName": "sap.ui.demo.walkthrough.view.App",
-                "type": "XML",
-                /*"async": true, // implicitly set via the sap.ui.core.IAsyncContentCreation interface*/
-                "id": "app"
-            }
+            metadata: {
+                interfaces: ["sap.ui.core.IAsyncContentCreation"],
+                manifest: "json"
+            },
         },
         init: function () {
             // call the init function of the parent
